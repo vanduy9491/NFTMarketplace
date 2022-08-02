678d4c8c58521193619af95fbf30728225eb37a4
Tran Van Duy <duytv@ait.com>
2022-08-02 16:24:02 +0700

    push

---
+++ components/index.js
@@ -0,0 +1,14 @@
+import HomeHeader from './HomeHeader'
+import NFTCard from './NFTCard'
+import FocusedStatusBar from './FocusedStatusBar'
+import { RectButton, CircleButton } from './Button'
+import { NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo } from './SubInfo'
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    RectButton,
+    CircleButton,
+    NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo
+}
---
+++ contants/index.js
@@ -0,0 +1,6 @@
+import assets from "./assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+import { NFTData } from "./dummy";
+
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
